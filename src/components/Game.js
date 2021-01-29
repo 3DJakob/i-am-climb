d6039456bd2cd3053d1b320e63f405193eeb4cd3
Jakob <jakob@folkdatorn.se>
2021-01-29 11:46:29 +0100

    Game component setup

---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Game () {
+  return (
+    <svg width='100vw' height='70vh'>
+      <rect width='100%' height='100%' fill='#00BEFF' />
+      <rect y='70%' width='100%' height='30%' fill='#287653' />
+    </svg>
+  )
+}
+export default Game
